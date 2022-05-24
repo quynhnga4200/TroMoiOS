ec5482fe9989f89695d0ceae11d1700cd79fa7c7
QuynhNga4200 <20211TT4200@mail.tdc.edu.vn>
2022-05-25 02:07:07 +0700

    Update layout

---
+++ TroMoiOS/ProfileController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileController.swift
+//  TroMoiOS
+//
+//  Created by PHAM QUYNH NGA on 25/05/2022.
+//
+
+import Foundation
---
+++ TroMoiOS/RegisterController.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterController.swift
+//  TroMoiOS
+//
+//  Created by PHAM QUYNH NGA on 25/05/2022.
+//
+
+import Foundation
