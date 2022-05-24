4e436aae11e89cd5524aa41f2f05a9ab4fc4c865
QuynhNga4200 <20211TT4200@mail.tdc.edu.vn>
2022-05-25 02:07:22 +0700

    update layout

--- TroMoiOS/RegisterController.swift
+++ TroMoiOS/RegisterController.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
+class RegisterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
