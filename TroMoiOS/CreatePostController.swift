edf53363c544a4021ff6e422b3b7240f51551e7b
QuynhNga4200 <20211TT4200@mail.tdc.edu.vn>
2022-05-26 15:18:56 +0700

    Update bắt sự kiện

--- TroMoiOS/CreatePostController.swift
+++ TroMoiOS/CreatePostController.swift
@@ -9,6 +9,8 @@ import Foundation
 import UIKit
 
 class CreatePostController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    var PostType: Int = 0
+    var RoomType: Int = 0
     @IBOutlet weak var PriceTextfield: UITextField!
     @IBOutlet weak var AcreageTextfield: UITextField!
     @IBOutlet weak var TitleTextfield: UITextField!
@@ -38,8 +40,10 @@ class CreatePostController: UIViewController,UITextFieldDelegate,UIImagePickerCo
         switch sender.selectedSegmentIndex {
             case 0:
                 print("Cho thuê")
+            PostType = sender.selectedSegmentIndex
             case 1:
                 print("Tìm người ở ghép")
+            PostType = sender.selectedSegmentIndex
             default:
                 break
             }
@@ -49,12 +53,16 @@ class CreatePostController: UIViewController,UITextFieldDelegate,UIImagePickerCo
         switch sender.selectedSegmentIndex {
             case 0:
                 print("Phòng")
+            RoomType = sender.selectedSegmentIndex
             case 1:
                 print("Căn hộ")
+            RoomType = sender.selectedSegmentIndex
             case 2:
                 print("Căn hộ mini")
+            RoomType = sender.selectedSegmentIndex
             case 3:
                 print("Nguyên căn")
+            RoomType = sender.selectedSegmentIndex
             default:
                 break
             }
@@ -77,5 +85,23 @@ class CreatePostController: UIViewController,UITextFieldDelegate,UIImagePickerCo
         }
     }
     
+    @IBAction func Confirmbutton(_ sender: UIButton) {
+        let bordercolor = UIColor.red
+        var check: Bool = false
+        if PriceTextfield.text == ""{
+            check = true
+            PriceTextfield.layer.borderColor = bordercolor.cgColor
+            PriceTextfield.layer.borderWidth = 1.0
+            
+        }
+        
+        if check  {
+            return
+        }
+        
+        print(PostType)
+        print(RoomType)
+        print(PriceTextfield.text)
+    }
 }
 
--- TroMoiOS/Room.swift
+++ TroMoiOS/Room.swift
@@ -7,21 +7,20 @@
 
 import Foundation
 import UIKit
-class Room {
-    //MARK: Properties
-    var Roomid: Int
-    var Roomtitle: String
-    var Roomowner: String
-    var Roomaddress: String
-    var Roomownerphone: String
-    var Roomacreage: Int
-    var RoomDescription: String
-    var Roomimage: [UIImage]?
-    var Roomutility:[UIImage]?
+//class Room {
+//    //MARK: Properties
+//    var Roomid: Int
+//    var Roomtitle: String
+//    var Roomowner: String
+//    var Roomaddress: String
+//    var Roomownerphone: String
+//    var Roomacreage: Int
+//    var RoomDescription: String
+//    var Roomimage: [UIImage]?
+//    var Roomutility:[UIImage]?
+//
+//    init(id: Int, title: String, owner: String, address: String, ownerphone: String, acreage: Int, description: String,image: [UIImage]?, untility: [UIImage]?) {
+//    }
     
-    init(id: Int, title: String, owner: String, address: String, ownerphone: String, acreage: Int, description: String,image: [UIImage]?, untility: [UIImage]?) {
-        <#statements#>
-    }
     
-    
-}
+//}
