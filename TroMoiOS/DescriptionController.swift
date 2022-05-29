67738ad0dc5ae39dbdaba402d11730b62977a812
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-05-29 17:02:47 +0700

    merge main

---
+++ TroMoiOS/DescriptionController.swift
@@ -0,0 +1,20 @@
+//
+//  DescriptionController.swift
+//  TroMoiOS
+//
+//  Created by PHAM QUYNH NGA on 25/05/2022.
+//
+
+import Foundation
+import UIKit
+
+class DescriptionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TroMoiOS/model/category.swift
@@ -0,0 +1,16 @@
+//
+//  category.swift
+//  TroMoiOS
+//
+//  Created by PHAM QUYNH NGA on 28/05/2022.
+//
+
+import Foundation
+class category{
+    var Categoryid : Int = 0
+    var Categoryname : String
+    init(cid : Int , cname : String) {
+        self.Categoryid = cid
+        self.Categoryname = cname
+    }
+}
