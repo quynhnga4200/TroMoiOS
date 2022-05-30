46733bd0cda2f143e7cf31391ba05a06cc1a53c5
QuynhNga4200 <20211TT4200@mail.tdc.edu.vn>
2022-05-30 16:18:31 +0700

    up

--- TroMoiOS/HomeController.swift
+++ TroMoiOS/HomeController.swift
@@ -9,19 +9,43 @@ import Foundation
 import UIKit
 
 class HomeController: UIViewController , UITableViewDelegate , UITableViewDataSource {
-    
-    
+    var rooms = [Room]()
+    var HomeCellarray = [HomeTableViewCell]()
+    //var image = UIImageView(named:"Logo")
     @IBOutlet weak var tableView: UITableView!
-    
-    let addressData = ["Logo" , "phone" , "time"]
-    let areaData = ["50" , "5" , "10"]
-    let price = ["4" , "9" , "12"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return userData.count
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as! Home
+        return cell
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // dang ki nhan dang tai su dung o
-        //let nib = UINib(nibName: "HomeTableView", bundle: nil)
+        var request = URLRequest(url: URL(string: "http://127.0.0.1/APIMobile/?type=1")!)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let task = session.dataTask(with: request, completionHandler: { [self] data, response, error -> Void in
+            //print(response!)
+            DispatchQueue.main.async {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!) as! [Dictionary<String, AnyObject>]
+                    print(json[1]["tienIch"]!)
+                    for item in json {
+                        print(item)
+                    }
+                } catch {
+                    print("error")
+                }
+                  }
+
+            
+        })
         
-        //tableView.register(nib, forCellReuseIdentifier: "HomeTableView")
+        task.resume()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.allowsSelection = false
@@ -29,26 +53,38 @@ class HomeController: UIViewController , UITableViewDelegate , UITableViewDataSo
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 120
     }
-    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return addressData.count
+        return rooms.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableView", for: indexPath) as! HomeTableViewCell
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "newRoom") as? HomeTableViewCell{
+            let room = rooms[indexPath.row]
+            cell.motelIMG.image = room.Roomimage[indexPath.row]
+            cell.addressLB.text = room.Roomaddress
+            cell.areaLB.text = room.Roomacreage
+            cell.priceLB.text = room.Roomprice
+            return cell
+            
+        }
+        else{
+            fatalError("Can not create the cell")
+        }
+      
         
-        //lay duong dan trong mang
-        let motel = addressData[indexPath.row]
-        let area = areaData[indexPath.row]
-        let price = price[indexPath.row]
-        
-        
-        cell.motelIMG.image = UIImage (named: motel)
-        cell.addressLB.text = motel
-        cell.areaLB.text = "DT: \(area) m2"
-        cell.priceLB.text = "\(price) triệu"
-      return cell
-        
+    }
+    @IBAction func unWindFromCreatePostController (segue: UIStoryboardSegue){
+        print("Hello")
+        if let sourceController = segue.identifier as? CreatePostController{
+            if let room = sourceController.room{
+                rooms += [room]
+                let newindexPath = IndexPath(row: rooms.count - 1, section: 0)
+                tableView.insertRows(at: [newindexPath], with: .automatic)
+            }
+        }
     }
    
 }
