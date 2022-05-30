3d2f106a1a1a6fccaee3a4b72dc1f25f946c2210
TranQuocHuya <86847384+TranQuocHuya@users.noreply.github.com>
2022-05-30 14:36:08 +0700

    up tapbar

--- TroMoiOS/model/Room.swift
+++ TroMoiOS/model/Room.swift
@@ -16,19 +16,27 @@ class Room {
     var Roomownerphone: String = ""
     var Roomacreage: Int = 0
     var RoomDescription: String = ""
-    var Roomimage: [UIImage]?
-    var Roomutility:[UIImage]?
-
-    init(id: Int, title: String, owner: String, address: String, ownerphone: String, acreage: Int, description: String,image: [UIImage]?, untility: [UIImage]?) {
-        self.Roomid = id
-        self.Roomtitle = title
-        self.Roomowner = owner
-        self.Roomaddress = address
-        self.Roomownerphone = ownerphone
-        self.Roomacreage = acreage
-        self.RoomDescription = description
-        self.Roomimage = image
-        self.Roomutility = untility
+    //var Roomimage: [UIImage]?
+    //var Roomutility:[UIImage]?
+    var RoomPrice: Int = 0
+    var Roomimg:UIImage?
+    //init(id: Int, title: String, owner: String, address: String, ownerphone: String, acreage: Int, description: String//,image: [UIImage]?, untility: [UIImage]?) {
+       // self.Roomid = id
+       // self.Roomtitle = title
+       // self.Roomowner = owner
+       // self.Roomaddress = address
+        //  self.Roomownerphone = ownerphone
+       // self.Roomacreage = acreage
+       // self.RoomDescription = description
+        //self.Roomimage = image
+        //self.Roomutility = untility
+    //}
+    
+    init?( motelImg: UIImage?, address: String,acreage: Int , price : Int  ) {
+    self.Roomaddress = address
+    self.Roomimg = motelImg
+    self.Roomacreage = acreage
+    self.RoomPrice = price
     }
 }
     
